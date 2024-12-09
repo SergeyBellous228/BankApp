75a9c19ecb700f1e70d48dbd3b5377ca69dcd1b8
User <sergey.belous228@gmail.com>
2024-12-09 15:52:36 +0200

    start

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+
+class User {};
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
